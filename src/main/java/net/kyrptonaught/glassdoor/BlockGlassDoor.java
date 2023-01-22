fe485f42d9891fb2a36f7a6cf3bbac0f3e013ad2
Spencer <75862693+TacoMonkey11@users.noreply.github.com>
2023-01-21 20:38:22 -0500

    1.19.3 port

--- src/main/java/net/kyrptonaught/glassdoor/BlockGlassDoor.java
+++ src/main/java/net/kyrptonaught/glassdoor/BlockGlassDoor.java
@@ -1,18 +1,22 @@
 package net.kyrptonaught.glassdoor;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.TallBlockItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 
 public class BlockGlassDoor extends DoorBlock {
 
     BlockGlassDoor(Settings block$Settings_1, String name) {
-        super(block$Settings_1);
-        Registry.register(Registry.BLOCK, new Identifier(GlassDoorMod.MOD_ID, name), this);
-        Registry.register(Registry.ITEM, new Identifier(GlassDoorMod.MOD_ID, name), new TallBlockItem(this, new Item.Settings().group(ItemGroup.REDSTONE)));
+        super(block$Settings_1, SoundEvents.BLOCK_WOODEN_DOOR_CLOSE, SoundEvents.BLOCK_WOODEN_DOOR_OPEN);
+        Registry.register(Registries.BLOCK, new Identifier(GlassDoorMod.MOD_ID, name), this);
+        Registry.register(Registries.ITEM, new Identifier(GlassDoorMod.MOD_ID, name), new TallBlockItem(this, new Item.Settings()));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(entries -> entries.add(this));
     }
 }
